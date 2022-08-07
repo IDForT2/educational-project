85fb6976a9502afa3df5d16244c71e6759137d95
Danil <madcobra918@gmail.com>
2022-08-07 19:16:17 +0300

    Working with Lists and Components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, {useState} from 'react'
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-    const [likes, setLikes] = useState(0);
-    const [value, setValue] = useState('Текст в Инпуте');
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "Description"},
+        {id: 2, title: "React", body: " React Description"},
+        {id: 3, title: "Javascript 2", body: "Description"},
+    ])
 
-    function increment() {
-        setLikes(likes + 1)
+    const [title, setTitle] = useState('')
+
+    const addNewPost = (e) => {
+        e.preventDefault()
     }
 
-    function decrement() {
-        setLikes(likes - 1)
-    }
-  return (
-    <div className="App">
-        <h1>{likes}</h1>
-        <h1>{value}</h1>
-        <input
-            type="text"
-            value={value}
-            onChange={event => setValue(event.target.value)}
-        />
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form>
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Post name"
+                />
+                <MyInput type="text" placeholder="Post description" />
+                <MyButton onClick={addNewPost} disabled>Add post</MyButton>
+            </form>
+            <PostList posts={posts} title="Posts about JS"/>
+        </div>
+    );
 }
 
 export default App;
