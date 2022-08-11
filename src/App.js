00e4c610d2315c0e70197860ab958086b2d01698
Danil <madcobra918@gmail.com>
2022-08-11 23:54:34 +0300

    Modal window

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, {useMemo, useRef, useState} from 'react'
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/Select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: "Javascript", body: "Description"},
-        {id: 2, title: "React", body: " React Description"},
-        {id: 3, title: "Javascript 2", body: "Description"},
+        {id: 1, title: "AAA", body: "Description"},
+        {id: 2, title: "ZZZ", body: " React Description"},
+        {id: 3, title: "JBBB", body: "Description"},
     ])
     const [filter, setFilter] = useState({sort: "", query: ""})
+    const [modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -22,11 +23,12 @@ function App() {
     }, [filter.sort, posts])
 
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
     }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     // Получаем ост из дочернего элемента
@@ -36,7 +38,12 @@ function App() {
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)} >
+                Create User
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter
                 filter={filter}
