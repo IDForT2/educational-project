bf2538710324c9f4359109cf16cfdd2aa1b33f03
Danil <madcobra918@gmail.com>
2022-08-13 20:40:50 +0300

    Axios and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useRef, useState} from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,6 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
     const [posts, setPosts] = useState([])
@@ -13,9 +14,18 @@ function App() {
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+    
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
     }
 
     // Получаем ост из дочернего элемента
@@ -25,6 +35,7 @@ function App() {
 
     return (
         <div className="App">
+            <button onClick={fetchPosts}>Get Posts</button>
             <MyButton style={{marginTop: 30}} onClick={() => setModal(true)} >
                 Create User
             </MyButton>
