d7e5fb791d42c76cdc0a03488a5b5ade4bb281b3
Danil <madcobra918@gmail.com>
2022-08-14 14:28:56 +0300

    Error processing and Pagination

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: "", query: ""})
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    let pagesArray = getPagesArray(totalPages);
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page);
+        setPosts(response.data)
+        const totalCount = (response.headers['x-total-count'])
+        setTotalPages(getPageCount(totalCount, limit))
+    })
 
     useEffect(() => {
         fetchPosts()
@@ -22,21 +37,21 @@ function App() {
         setPosts([...posts, newPost])
         setModal(false)
     }
-    
-    async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
-    }
 
-    // Получаем ост из дочернего элемента
+    // Получаем пост из дочернего элемента
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
+        fetchPosts()
     }
 
     return (
         <div className="App">
             <button onClick={fetchPosts}>Get Posts</button>
-            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)} >
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
                 Create User
             </MyButton>
             <MyModal visible={modal} setVisible={setModal}>
@@ -46,7 +61,23 @@ function App() {
             <PostFilter
                 filter={filter}
                 setFilter={setFilter}/>
-                <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts about JS"/>
+            {postError &&
+                <h1>Error ${postError}</h1>}
+            {isPostsLoading
+                ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader/></div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Posts about JS"/>
+            }
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span
+                        onClick={() => changePage(p)}
+                        key={p}
+                        className={page === p ? 'page page__current' : 'page'}
+                    >
+                        {p}
+                    </span>
+                )}
+            </div>
         </div>
     );
 }
